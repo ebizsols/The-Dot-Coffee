1a622c09d43368f549d97cc82dc6a828d01c4dca
Sadaqat Rasool <director@premiumebizsols.com>
2019-08-25 13:13:21 +0300

    Update config.js

--- www/js/config.js
+++ www/js/config.js
@@ -1,5 +1,5 @@
 var krms_config ={			
-	'ApiUrl':"154396879870-9ndphsghv62bd67cn0c00o3n8fledkir",			
+	'ApiUrl':"http://foodtech.eservices.gq/merchantapp/api",			
 	'DialogDefaultTitle':"KMRS",			
 	'MerchantKeys':"0c0911475809b8786a76a80c0f139194",
 	'debug': false
