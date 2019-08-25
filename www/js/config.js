cdfd24bab13ddd336b3632f7cbfbdedc3d060006
Sadaqat Rasool <director@premiumebizsols.com>
2019-08-25 12:33:17 +0300

    Initial commit

---
+++ www/js/config.js
@@ -0,0 +1,6 @@
+var krms_config ={			
+	'ApiUrl':"154396879870-9ndphsghv62bd67cn0c00o3n8fledkir",			
+	'DialogDefaultTitle':"KMRS",			
+	'MerchantKeys':"0c0911475809b8786a76a80c0f139194",
+	'debug': false
+};
