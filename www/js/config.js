f1481b46f5e47838de4825231e1ecc63375a1cfb
Sadaqat Rasool <director@premiumebizsols.com>
2020-03-21 23:39:40 +0300

    Branding updates

--- www/js/config.js
+++ www/js/config.js
@@ -1,6 +1,6 @@
 var krms_config ={			
 	'ApiUrl':"https://eateries.online/singlemerchant/api",			
-	'DialogDefaultTitle':"KARENDERIA",			
+	'DialogDefaultTitle':"The Dot Cofee",			
 	'MerchantKeys':"306f0ec73b7e428dbb8e0174971f49e9",
 	'debug': false
 };
