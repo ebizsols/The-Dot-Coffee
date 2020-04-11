c666036fa241b509f8bffaf1168ae4b702717d39
Sadaqat Rasool <director@premiumebizsols.com>
2020-04-11 19:46:00 +0300

    update

--- www/js/config.js
+++ www/js/config.js
@@ -1,6 +1,6 @@
 var krms_config ={			
 	'ApiUrl':"https://eateries.online/singlemerchant/api",			
-	'DialogDefaultTitle':"The Dot Cofee",			
+	'DialogDefaultTitle':"The Dot Coffee & Brunch App. Powered by Eateries.Online",			
 	'MerchantKeys':"306f0ec73b7e428dbb8e0174971f49e9",
 	'debug': false
 };
