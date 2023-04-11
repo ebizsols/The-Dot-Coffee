844c653f39e2eabd78fb0affa87f01009fdc9cbf
ebizsols <director@premiumebizsols.com>
2023-04-12 04:28:50 +0900

    Project's Initial Commit

---
+++ www/js/config.js
@@ -0,0 +1,6 @@
+var krms_config = {
+	'ApiUrl': "https://thedotcoffee.com/orders/singlemerchant/api",
+	'DialogDefaultTitle': "The Dot Coffee & Brunch App. Powered by Eateries.Online",
+	'MerchantKeys': "306f0ec73b7e428dbb8e0174971f49e9",
+	'debug': false
+};
